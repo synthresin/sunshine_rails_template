5ddfd3e31c3ac39c2dd3092ee7b88372e42f14be
synthresin <synthresin@bagseong-wan-ui-MacBook-Pro.local>
2014-05-01 23:24:42 +0900

    시작

--- template.rb
+++ template.rb
@@ -1,1 +1,37 @@
-template.rb
+gem "bj"
+gem "nokogiri"
+
+gem_group :development, :test do
+  gem "rspec-rails"
+end
+
+add_source "http://code.whytheluckystiff.net"
+
+environment 'config.action_mailer.default_url_options = {host: "http://yourwebsite.example.com"}', env: 'production'
+
+# template.rb
+generate(:scaffold, "person name:string")
+route "root to: 'people#index'"
+rake("db:migrate")
+ 
+git :init
+git add: "."
+git commit: %Q{ -m 'Initial commit' }
+
+
+initializer 'bloatlol.rb', <<-CODE
+  class Object
+    def not_nil?
+      !nil?
+    end
+ 
+    def not_blank?
+      !blank?
+    end
+  end
+CODE
+
+file 'app/components/foo.rb', <<-CODE
+  class Foo
+  end
+CODE
