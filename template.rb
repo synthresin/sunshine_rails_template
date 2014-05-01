6c98a61552a0b8501cdb159ea37cb747377d5944
synthresin <synthresin@bagseong-wan-ui-MacBook-Pro.local>
2014-05-01 23:31:05 +0900

    필요한 젬 추가

--- template.rb
+++ template.rb
@@ -1,37 +1,43 @@
-gem "bj"
-gem "nokogiri"
+gem 'jquery-ui-rails'
+gem 'devise'
+gem 'figaro'
+gem 'bootstrap-sass', '~> 3.1.1'
+gem 'will_paginate'
+gem 'carrierwave'
+gem 'rmagick'
+gem 'pry', group: :development
 
-gem_group :development, :test do
-  gem "rspec-rails"
-end
+# gem_group :development, :test do
+#   gem "rspec-rails"
+# end
 
-add_source "http://code.whytheluckystiff.net"
+# add_source "http://code.whytheluckystiff.net"
 
-environment 'config.action_mailer.default_url_options = {host: "http://yourwebsite.example.com"}', env: 'production'
+# environment 'config.action_mailer.default_url_options = {host: "http://yourwebsite.example.com"}', env: 'production'
 
-# template.rb
-generate(:scaffold, "person name:string")
-route "root to: 'people#index'"
-rake("db:migrate")
+# # template.rb
+# generate(:scaffold, "person name:string")
+# route "root to: 'people#index'"
+# rake("db:migrate")
  
-git :init
-git add: "."
-git commit: %Q{ -m 'Initial commit' }
+# git :init
+# git add: "."
+# git commit: %Q{ -m 'Initial commit' }
 
 
-initializer 'bloatlol.rb', <<-CODE
-  class Object
-    def not_nil?
-      !nil?
-    end
+# initializer 'bloatlol.rb', <<-CODE
+#   class Object
+#     def not_nil?
+#       !nil?
+#     end
  
-    def not_blank?
-      !blank?
-    end
-  end
-CODE
+#     def not_blank?
+#       !blank?
+#     end
+#   end
+# CODE
 
-file 'app/components/foo.rb', <<-CODE
-  class Foo
-  end
-CODE
+# file 'app/components/foo.rb', <<-CODE
+#   class Foo
+#   end
+# CODE
